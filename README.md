2626e8d64a70bd0d9b130a4997b79a03ce2763d8
CP Knight <cpknight@icloud.com>
2023-10-05 03:48:46 +0000

    Initial commit.

--- README.md
+++ README.md
@@ -1,2 +1,53 @@
 # Retro-BBS-Term
 My script for connecting to retro BBSs on linux using qodem and tcpser for a bit of added retro-authenticity.
+
+I'm running this on a Pop_OS! linux, and the `retroBBSterm.sh` script probably won't work super-well on yours without some modification. In particular, I've hard-coded a number of references to my home directory (`/home/cpknight`) which I'm guessing won't likely work well for you.
+
+## Acknoweldgements...
+
+- The `tcpser` here was ripped from [go4retro/tcpser](https://github.com/go4retro/tcpser) which itself is a fork from Jim Brian's tcpser. This is a modem emulator which give me the experience of using `AT` commands so that I can pretend to 'dial' BBS's the old-school way. I do appreciate this is somewhat redundant to the built-in capabilities of `qodem`, but this setup works well for me. 
+	- Some other versions are floating around, eg. [FozzTexx/tcpser](https://github.com/fozztexx/tcpser). I'll be checking out the other versions later on to see if I can improve the functionality of this script. In due course. 
+
+- The `qodem` here was ripped from [quodem.soutceforge.io](https://qodem.sourceforge.io/). Thanks to Autumn Lamonte and all contributors for this amazing version Qmodem, which I remember from my BBS'ing days in the 1990s! 
+	- Some other versions are floating around which I should take a look at some point, eg. [cnmade/qodem](https://github.com/cnmade/qodem).
+
+## Building `tcpser` and `qodem` and installing `socat`...
+
+The script assumes that `tcpser` and `qodem` are subdirectories containing built binaries in sub-directories. I don't install these two tools into `/usr/local/bin` or anything, since my use of these two execcutables is pretty self-contained. 
+
+
+
+- `tcpser` is easy to build; from the `Retro-BBS-Term` directory, probably just `cd tcpser`, `make clean` and `make` is all you need. This repo contains artifacts from the last time I built tcpser, so you'll definitely want to clean and re-build this.
+
+- `qodem` is a bit trickier to build since you'll have to clean artifacts up and run `configure` before building. I had to install some libraries that were not included in my distro. :warning: - running `qodem` by default creates all sorts of stuff in your home directory, so if you don't want that, it might be best to just run it through the script. From the `Retro-BBS-Term` directory:
+	- `sudo apt install lib32ncurses-dev`
+	- `sudo apt install ibgpm-dev`
+	- `cd qodem ; ./configure`
+	- `make clean ; make`
+
+- `socat` also needs to be installed, and this isn't the default on my distro:
+	- `sudo apt install socat`
+
+## Using the `retroBBSterm.sh` script...
+
+- I make a symbolic link to the `retroBBSterm.desktop` file so that I can double-click on an icon from my gnome desktop: 
+```
+ln -s /home/cpknight/Projects/Retro-BBS-Term/retroBBSterm.desktop /usr/share/applications/retroBBSterm.desktop
+```
+
+- When you first get into `qodem` you'll have to connect it to the 'modem' - use `Alt-9` to do this. An error message will pop up on the screen, which can be dismissed. Now you're talking to the modem. Use `AT` commands, eg:
+	- `AT` should respond with an `OK`
+	- `ATDT shsbbs.net` to 'dial' ....
+	- `ATDT canadianrebel.sytes.net:1981` to 'dial' ....
+	- `+++` while connected to get back to the 'modem' (eg. then do `ATH` to hangup).
+
+- You should be able to use all of the functionality of `qodem`, but I have some notes and 'todos' below to work on.
+	- :warning: Don't toggle the status line mode using `Alt-7` - this gets caught up in an error loop, which I'll have to look into and fix later on.
+
+## Todo's...
+
+- Remove hard-coded paths and make the script user-generic.
+- Dig into things and improve the interaction between `socat` and `qodem`.
+	- Probably related: `zmodem` file transfers don't work.
+- Fix up this repo and add build and install scripts, etc.
+
---
+++ retroBBSterm.sh
@@ -0,0 +1,105 @@
+#!/usr/bin/bash
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Change to the hard-coded directory for this project.
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+cd /home/cpknight/Projects/Retro-BBS-Term
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Make a temporary directory
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+TMP_DIRECTORY="$(mktemp -d /tmp/retroBBSterm.$BASHPID.XXX)"
+
+echo "    Temporary Dir: ${TMP_DIRECTORY}"
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Determine an available port in the user range.
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+THE_PORT=$(python3 -c 'import socket; s=socket.socket(); s.bind(("", 0)); print(s.getsockname()[1]); s.close()');
+echo "   Available Port: ${THE_PORT}"
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Start tcpser on that port.
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+CMD_TCPSER="tcpser/tcpser -v $THE_PORT"
+$CMD_TCPSER >/dev/null 2>&1 &
+PID_TCPSER=$!  
+echo "       PID/tcpser: ${PID_TCPSER}"
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Come up with a cool name for the pty device
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+PTY_PATH=${TMP_DIRECTORY}/hayes
+echo "    Virtual modem: ${PTY_PATH}"
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Start socat
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+CMD_SOCAT="socat -d -d -v pty,rawer,link=${PTY_PATH} TCP:127.0.0.1:${THE_PORT}"
+$CMD_SOCAT >${TMP_DIRECTORY}/socat.log 2>&1 &
+PID_SOCAT=$!  
+echo "        PID/socat: ${PID_SOCAT}"
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# setup qodem
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+echo "# Qodem modem configuration file for retroBBSterm! " > ${TMP_DIRECTORY}/modem.cfg
+echo "# " >> ${TMP_DIRECTORY}/modem.cfg
+echo "name = Hayes Modem " >> ${TMP_DIRECTORY}/modem.cfg
+echo "dev_name = ${PTY_PATH} " >> ${TMP_DIRECTORY}/modem.cfg
+echo "lock_dir = /var/lock " >> ${TMP_DIRECTORY}/modem.cfg
+echo "init_string = " >> ${TMP_DIRECTORY}/modem.cfg
+echo "hangup_string = +~+~+~~~~ATH0^M " >> ${TMP_DIRECTORY}/modem.cfg
+echo "dial_string = ATDT " >> ${TMP_DIRECTORY}/modem.cfg
+echo "host_init_string = ATE1Q0V1M1H0S0=0^M " >> ${TMP_DIRECTORY}/modem.cfg
+echo "answer_string = ATA^M " >> ${TMP_DIRECTORY}/modem.cfg
+echo "baud = 38400 " >> ${TMP_DIRECTORY}/modem.cfg
+echo "data_bits = 8 " >> ${TMP_DIRECTORY}/modem.cfg
+echo "parity = none " >> ${TMP_DIRECTORY}/modem.cfg
+echo "stop_bits = 1 " >> ${TMP_DIRECTORY}/modem.cfg
+echo "xonxoff = false " >> ${TMP_DIRECTORY}/modem.cfg
+echo "rtscts = true " >> ${TMP_DIRECTORY}/modem.cfg
+echo "lock_dte_baud = true " >> ${TMP_DIRECTORY}/modem.cfg
+echo "ignore_dcd = false " >> ${TMP_DIRECTORY}/modem.cfg
+echo " " >> ${TMP_DIRECTORY}/modem.cfg
+
+echo "working_dir = ${TMP_DIRECTORY}" >> ${TMP_DIRECTORY}/qodemrc
+echo "host_mode_dir = ${TMP_DIRECTORY}" >> ${TMP_DIRECTORY}/qodemrc
+echo "download_dir = /home/cpknight/Downloads" >> ${TMP_DIRECTORY}/qodemrc
+echo "upload_dir = /home/cpknight/Downloads" >> ${TMP_DIRECTORY}/qodemrc
+echo "scripts_dir = ${TMP_DIRECTORY}  " >> ${TMP_DIRECTORY}/qodemrc
+echo "scripts_fifo = ${TMP_DIRECTORY}/script.stderr" >> ${TMP_DIRECTORY}/qodemrc
+echo "bew_file = ${TMP_DIRECTORY}/batch_upload.txt " >> ${TMP_DIRECTORY}/qodemrc
+echo "editor = subl" >> ${TMP_DIRECTORY}/qodemrc
+echo "start_in_phonebook = false" >> ${TMP_DIRECTORY}/qodemrc
+echo " " >> ${TMP_DIRECTORY}/qodemrc
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# launch qodem in a new, special kitty instance
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+kitty --override font_size=18 \
+	--override remember_window_size=no \
+	--override initial_window_width=82c \
+	--override initial_window_height=26c \
+	qodem/bin/qodem --dotqodem-dir ${TMP_DIRECTORY} --config ${TMP_DIRECTORY}/qodemrc --emulation ansi >${TMP_DIRECTORY}/qodem.log 2>&1
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Kill the tcpser and socat processes 
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+kill $PID_TCPSER
+# kill $PID_SOCAT    # unnecessary as it kills itself after tcpser exit
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Remove the temporary directory
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+rm -rf ${TMP_DIRECTORY} >/dev/null 2>&1
